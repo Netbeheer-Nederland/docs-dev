5990bb851a9bfba55604dafd4a37839e21025e2a
Bart Kleijngeld <bartkl@gmail.com>
2025-02-12 11:51:20 +0100

    Fixed admonition block issues

--- supplemental-ui/js/wrap-tables.js
+++ supplemental-ui/js/wrap-tables.js
@@ -1,4 +1,4 @@
-document.querySelectorAll("table").forEach( function (table) {
-    var container = Object.assign(document.createElement('div'), { className: 'table-container' })
+document.querySelectorAll("table.tableblock").forEach( function (table) {
+    var container = Object.assign(document.createElement("div"), { className: "table-container" })
     table.parentNode.insertBefore(container, table).appendChild(table)
 })
